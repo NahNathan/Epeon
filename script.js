26fcf9f12689c20eb87833164c56d9d88a6254b0
Nathan Rodrigues dos Santos <97409681+NahNathan@users.noreply.github.com>
2022-12-27 12:43:04 -0300

    Melhorias

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function rolada(){
     let i = Math.floor((Math.random() * 20) + 1)
-    document.getElementById("rolada").innerHTML="Rode um dado:",i
-    alert("CLICADO!!")
+
+    if(i<10)
+        document.getElementById("rolada").innerHTML="Rode um dado   :"+i
+    else
+        document.getElementById("rolada").innerHTML="Rode um dado:"+i
+    console.log("CLICADO!!")
 }
